c40070d94dbf3132c5ce22b2c93a0d863063f90f
Yaroslav <yaroslavv86@gmail.com>
2023-10-04 12:02:56 +0600

    fix

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -20,6 +20,7 @@ const Contact = () => {
    return (
       <section id="contact" className="py-48">
          <motion.div
+            className="flex justify-end w-full"
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true, amount: 0.5 }}
@@ -29,16 +30,107 @@ const Contact = () => {
                visible: { opacity: 1, y: 0 },
             }}
          >
-            <div className="relative h-32">
-               <div className="z-10">
-                  <p className="font-playfair font-semibold text-5xl">01</p>
-                  <p className="font-playfair font-semibold text-3xl mt-3">
-                     Imaganative
-                  </p>
+            <div>
+               <p className="font-playfair font-semibold text-4xl">
+                  <span className="text-yellow">CONTACT ME</span> TO GET STARTED
+               </p>
+               <div className="flex md:justify-end my-5">
+                  <LineGradient width="w-1/2" />
                </div>
-               <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
             </div>
          </motion.div>
+         <div className="md:flex md:justify-between gap-16 mt-5">
+            <motion.div
+               className="basis-1/2 flex justify-center"
+               initial="hidden"
+               whileInView="visible"
+               viewport={{ once: true, amount: 0.5 }}
+               transition={{ duration: 0.5 }}
+               variants={{
+                  hidden: { opacity: 0, y: 50 },
+                  visible: { opacity: 1, y: 0 },
+               }}
+            >
+               <img src="../assets/contact-image.jpeg" alt="contact" />
+            </motion.div>
+
+            <motion.div
+               className="basis-1/2 mt-10 md:mt-0"
+               initial="hidden"
+               whileInView="visible"
+               viewport={{ once: true, amount: 0.5 }}
+               transition={{ delay: 0.2, duration: 0.5 }}
+               variants={{
+                  hidden: { opacity: 0, y: 50 },
+                  visible: { opacity: 1, y: 0 },
+               }}
+            >
+               <form
+                  action="https://formsubmit.co/77bfd51512e6a420d0dd6aec0815ea03 "
+                  target="_blank"
+                  onSubmit={() => {}}
+                  method="POST"
+               >
+                  <input
+                     type="text"
+                     className="w-full bg-blue placeholder-opaque-black p-3"
+                     placeholder="NAME"
+                     {...register("name", {
+                        required: true,
+                        maxLength: 100,
+                     })}
+                  />
+                  {errors.name && (
+                     <p className="text-red mt-1">
+                        {errors.name.type === "required" &&
+                           "This is field is required."}
+                        {errors.name.type === "maxLength" &&
+                           "This is is 100 char."}
+                     </p>
+                  )}
+                  <input
+                     className="w-full bg-blue placeholder-opaque-black p-3 mt-5"
+                     placeholder="EMAIL"
+                     {...register("email", {
+                        required: true,
+                        pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                     })}
+                  />
+                  {errors.email && (
+                     <p className="text-red mt-1">
+                        {errors.name.type === "required" &&
+                           "This is field is required."}
+                        {errors.name.type === "pattern" &&
+                           "Invalid email address."}
+                     </p>
+                  )}
+                  <textarea
+                     className="w-full bg-blue placeholder-opaque-black p-3 mt-5"
+                     placeholder="MESSAGE"
+                     rows="4"
+                     cols="50"
+                     {...register("message", {
+                        required: true,
+                        maxLength: 2000,
+                     })}
+                  />
+                  {errors.message && (
+                     <p className="text-red mt-1">
+                        {errors.name.type === "required" &&
+                           "This is field is required."}
+                        {errors.name.type === "maxLength" &&
+                           "This is 2000 char."}
+                     </p>
+                  )}
+                  <button
+                     type="submit"
+                     className="p-5 bg-yellow font-semibold text-deep-blue mt-5 hover:bg-red hover:text-white transition duration-500"
+                  >
+                     SEND ME A MESSAGE
+                  </button>
+               </form>
+            </motion.div>
+         </div>
       </section>
    );
 };
