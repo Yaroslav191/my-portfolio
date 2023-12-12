36369406a60754c079beed0f2f968fee32ba5250
Yaroslav <yaroslavv86@gmail.com>
2023-12-13 01:12:18 +0600

    fix

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -15,13 +15,13 @@ const projectVariant = {
 };
 
 const Project = ({ title, text, link }) => {
-  const overlayStyles = `absolute h-[400px] opacity-0 hover:opacity-90 transition
+  const overlayStyles = `hover:cursor-pointer absolute h-[400px] opacity-0 hover:opacity-90 transition
     duration-500 bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
 
   return (
     <motion.div variants={projectVariant} className="relative">
-      <a href={link} target="_blank">
+      <a href={link} target="_blank" rel="noreferrer">
         <div className={overlayStyles}>
           <p className="text-2xl font-playfair">{title}</p>
           <p className="mt-7">{text}</p>
@@ -66,7 +66,7 @@ const Projects = () => {
 
       <div className="flex justify-center">
         <motion.div
-          className="flex flex-wrap justify-center"
+          className="sm:grid sm:grid-cols-3"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
@@ -76,6 +76,8 @@ const Projects = () => {
                 text-2xl font-playfair font-semibold">
             BEAUTIFUL USER INTERFACES
           </div>
+          {/* <Project title="Project 1" />
+          <Project title="Project 2" /> */}
           <Project
             title="Project 1"
             text={
@@ -95,9 +97,17 @@ const Projects = () => {
             text={
               "Step into a world of seamless organization and enhanced productivity with our custom Trello application."
             }
-            link={"https://trello-87jdtt6z7-godlikes-projects.vercel.app/"}
+            link={
+              "https://trello-yaroslavv86-gmailcom-godlikes-projects.vercel.app/"
+            }
           />
-
+          <Project
+            title="Project 1"
+            text={
+              "Stay connected anytime, anywhere, and make every conversationmemorable with our dynamic chat experience."
+            }
+            link={"https://messanger-godlikes-projects.vercel.app/"}
+          />
           <div
             className="flex justify-center text-center items-center p-10 bg-blue w-[350px] md:w-[400px] h-[400px]
                 text-2xl font-playfair font-semibold">
