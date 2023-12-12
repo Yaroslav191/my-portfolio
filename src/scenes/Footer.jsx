59379461033818369f99acbba3e7d3271083e54a
Yaroslav <yaroslavv86@gmail.com>
2023-12-12 23:41:48 +0600

    fix

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import SocialMediaIcons from "../components/SocialMediaIcons";
 
 const Footer = () => {
-   return (
-      <footer className="h-64 bg-red pt-10">
-         <div className="w-5/6 mx-auto">
-            <SocialMediaIcons />
-            <div className="md:flex justify-center md:justify-between text-center">
-               <p className="font-playfair font-semibold text-2xl text-yellow">
-                  Yaroslav Trofimov 
-               </p>
-               <p className="font-playfair text-md text-yellow">
-                  @2023 ESPER. ALL Right Reserved
-               </p>
-            </div>
-         </div>
-      </footer>
-   );
+  return (
+    <footer className="h-64 bg-red pt-10">
+      <div className="w-5/6 mx-auto">
+        <SocialMediaIcons />
+        <div className="md:flex justify-center md:justify-between text-center">
+          <p className="font-playfair font-semibold text-2xl text-yellow">
+            Yaroslav Trofimov
+          </p>
+          <p className="font-playfair text-md text-yellow">
+            @2023 Yaroslav. ALL Right Reserved
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
