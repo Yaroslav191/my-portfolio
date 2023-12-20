8b102cce3ea8ecd1da3e58d34429601a88a095b6
Yaroslav <yaroslavv86@gmail.com>
2023-12-20 10:02:56 +0600

    fix

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,33 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const SocialMediaIcons = () => {
-  return (
-    <div className="flex justify-center md:justify-start my-10 gap-7">
-      <a
-        href="https://www.linkedin.com/in/yaroslav-trofimov-b03244230/"
-        className="hover:opacity-50 transition duration-500"
-        target="_blank"
-        rel="noreferrer">
-        <i className="fa-brands fa-linkedin text-4xl"></i>
-      </a>
-      <a
-        href="https://www.instagram.com/yarosslav__14/"
-        className="hover:opacity-50 transition duration-500 "
-        target="_blank"
-        rel="noreferrer">
-        <i className="fa-brands fa-instagram text-4xl"></i>
-        {/* <img src="../assets/instagram.png" alt="instagram-link" /> */}
-      </a>
-      <a
-        href="https://www.instagram.com/yarosslav__14/"
-        className="hover:opacity-50 transition duration-500 "
-        target="_blank"
-        rel="noreferrer">
-     <i class="fa-brands fa-github text-4xl"></i>
-      </a>
-      
-    </div>
-  );
+   return (
+      <div className="flex justify-center md:justify-start my-10 gap-7">
+         <a
+            href="https://www.linkedin.com/in/yaroslav-trofimov-b03244230/"
+            className="hover:opacity-50 transition duration-500"
+            target="_blank"
+            rel="noreferrer"
+         >
+            <i className="fa-brands fa-linkedin text-4xl"></i>
+         </a>
+         <a
+            href="https://www.instagram.com/yarosslav__14/"
+            className="hover:opacity-50 transition duration-500 "
+            target="_blank"
+            rel="noreferrer"
+         >
+            <i className="fa-brands fa-instagram text-4xl"></i>
+            {/* <img src="../assets/instagram.png" alt="instagram-link" /> */}
+         </a>
+         <a
+            href="https://github.com/Yaroslav191"
+            className="hover:opacity-50 transition duration-500 "
+            target="_blank"
+            rel="noreferrer"
+         >
+            <i class="fa-brands fa-github text-4xl"></i>
+         </a>
+      </div>
+   );
 };
 
 export default SocialMediaIcons;
